b45a9f53af4a9fcccf346b836b1c93c598328ce9
Nat Waterworth <32358976+ProgrammerWaterworth@users.noreply.github.com>
2021-08-02 12:14:48 +0100

    Reset Project to 2D (was 3D)

---
+++ Mobile Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Touch startingPoint, endPoint;
+    bool drag, release, canMove;
+
+    Rigidbody2D rb;
+    [SerializeField] float forceMultuplier;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+            UpdateTouchControl();
+    }
+
+
+    void UpdateTouchControl()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (!drag)
+            {
+                startingPoint = Input.touches[0];
+                drag = true;
+            }
+            endPoint = Input.touches[0];
+        }
+        else if (drag)
+        {
+            Release();
+            drag = false;
+        }
+            
+    }
+
+    void Release()
+    {
+        Vector3 _startPoint = Camera.main.ScreenToWorldPoint(startingPoint.position);
+        Vector3 _endPoint = Camera.main.ScreenToWorldPoint(endPoint.position);
+
+        Vector3 _force = (_endPoint - _startPoint) * forceMultuplier;
+
+        rb.AddForce(_force, ForceMode2D.Impulse);
+
+        //canMove = false;
+    }
+}
