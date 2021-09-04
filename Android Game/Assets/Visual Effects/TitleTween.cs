d4e317ed1c6f59de932e6841ae25ee60c0e13f11
Nat Waterworth <32358976+ProgrammerWaterworth@users.noreply.github.com>
2021-09-04 17:19:16 +0100

    Wobbly Main Menu Title

---
+++ Android Game/Assets/Visual Effects/TitleTween.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class TitleTween : MonoBehaviour
+{
+    [SerializeField] float duration;
+    [SerializeField] LeanTweenType easeType;
+    [SerializeField] float delay;
+    [SerializeField] float moveY;
+
+
+    private void OnEnable()
+    {
+        if (GetComponent<RectTransform>() != null)
+        {           
+            //GetComponent<RectTransform>().position -= new Vector3(0, moveY, 0);
+            LeanTween.moveY(gameObject, GetComponent<RectTransform>().rect.y + moveY, duration).setEase(easeType).setIgnoreTimeScale(true).setLoopPingPong().setDelay(delay);
+          
+        }
+    }
+
+    void OnDisable()
+    {
+        LeanTween.reset();
+    }
+
+}
+
